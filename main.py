9813997c7200a84f57440fef7b655136b1d2830c
artemzenov <113045342+artemzenov@users.noreply.github.com>
2024-08-24 14:37:23 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+count_homework = 12
+count_spent_hour = 1.5
+name_course = 'Python'
+time_one_homework = count_spent_hour / count_homework
+print('Курс:', name_course + ',',
+      'всего задач:', str(count_homework) + ',',
+      'затрачено часов:', str(count_spent_hour) + ',',
+      'среднее время выполнения', time_one_homework, 'часа')
